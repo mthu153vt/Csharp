1634e2e68ba09fed0d204f3bc8da40f0ac4ec33c
Thư Lê <81005644+mthu153vt@users.noreply.github.com>
2021-09-13 11:46:23 +0700

    Add files via upload

---
+++ Bai 1/CSharp Excercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("  [Kiem tra so nguyen to] \n");
+
+            int number;
+            int count = 0;
+            Console.Write("Nhap n: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % 1 == 0)
+                    count++;
+            }
+            if (count == 2)
+                Console.WriteLine("-> So da cho la so nguyen to: ");
+            else
+                Console.WriteLine("-> So da cho khong phai la so nguyen to.");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Bai 2/Bai 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Bai_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int j, number1, number2;
+			int USCLN = 1;
+
+			Console.Write("[Tim uoc chung lon nhat] \n");
+			Console.Write("\n");
+			Console.Write("Nhap so thu nhat: ");
+			number1 = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Nhap so thu hai: ");
+			number2 = Convert.ToInt32(Console.ReadLine());
+			j = (number1 < number2) ? number1 : number2;
+			for (int i = 1; i <= j; i++)
+			{
+				if (number1 % i == 0 && number2 % i == 0)
+				{
+					USCLN = i;
+				}
+			}
+			Console.Write("USCLN cua {0},{1} la: {2}", number1, number2, USCLN);
+			Console.ReadKey();
+		}
+    }
+}
